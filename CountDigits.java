37799e4b37c26289d80cde7c1fecc849ca113ba8
Manish kumar <40349915+manish7739@users.noreply.github.com>
2021-09-21 07:33:39 -0400

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,25 @@
+package Task2Day;
+
+import java.util.Scanner;
+
+public class CountDigits {
+	public static void main(String args[]) {
+		int a[]=new int[5];
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter element of array");
+		for(int i=0; i<a.length; i++)
+		{
+			a[i]=sc.nextInt();
+			
+		}
+		System.out.println("array Element : ");
+		for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]+" ");
+			
+		}
+		System.out.println("array lenght  : " +a.length);
+		}
+
+}
